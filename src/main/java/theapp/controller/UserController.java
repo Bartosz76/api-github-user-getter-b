834fff0cd31c25f68cb5a42954f012ef5f7497c7
Dazikiri Anikar <mierzwinski.b@gmail.com>
2020-08-30 19:04:20 +0200

    Added a method inserting the record (a login and the number of HTTTP GET calls for that login) into a database.

--- src/main/java/theapp/controller/UserController.java
+++ src/main/java/theapp/controller/UserController.java
@@ -1,6 +1,7 @@
 package theapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import theapp.model.User;
@@ -18,12 +19,11 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/info")
-    public User getUser() throws IOException {
+    @GetMapping("/user/info/{login}")
+    public User getUser(@PathVariable String login) throws IOException {
+        userService.insertRecord(login);
         return userService.fetchUser();
     }
-
-
 
 
 }
--- src/main/java/theapp/service/UserService.java
+++ src/main/java/theapp/service/UserService.java
@@ -3,11 +3,19 @@ package theapp.service;
 import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import theapp.config.Connector;
+import theapp.config.Constants;
 import theapp.model.User;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static theapp.config.Constants.*;
 
 @org.springframework.stereotype.Service
 public class UserService {
@@ -16,7 +24,7 @@ public class UserService {
 
     public User fetchUser() throws IOException {
 
-        URL url = new URL("https://api.github.com/users/octocat");
+        URL url = new URL("https://api.github.com/users");
         InputStreamReader reader = new InputStreamReader(url.openStream());
 
         User user = new Gson().fromJson(reader, User.class);
@@ -27,5 +35,50 @@ public class UserService {
             logger.info("The output returned.");
             return user;
         }
+    }
+
+    public void insertRecord(String login) {
+        String sql = "select * from calls where LOGIN = ?";
+        PreparedStatement preparedStatement;
+        ResultSet resultSet;
+        try {
+            preparedStatement = getConnection().prepareStatement(sql);
+            preparedStatement.setString(1, login);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                String updateSql = "update calls set REQUEST_COUNT = REQUEST_COUNT + 1 where login = ?";
+                PreparedStatement updatePreparedStatement;
+                try {
+                    updatePreparedStatement = getConnection().prepareStatement(updateSql);
+                    updatePreparedStatement.setString(1, login);
+                } catch (SQLException e) {
+                    logger.error("Could not insert a record into the database.");
+                    e.printStackTrace();
+                }
+            } else {
+                String insertSql = "insert into calls (LOGIN, REQUEST_COUNT) values (?, ?)";
+                try (final PreparedStatement insertPreparedStatement = getConnection().prepareStatement(insertSql)) {
+                    insertPreparedStatement.setString(1, login);
+                    insertPreparedStatement.setInt(1, 1);
+                } catch (SQLException e) {
+                    logger.error("Could not insert a record into the database.");
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (SQLException e) {
+            logger.error("Operation failed.");
+            e.printStackTrace();
+        }
+    }
+
+
+    private Connection getConnection() {
+        Connection connection = Connector.createConnection(DEFAULT_DRIVER, DEFAULT_URL, DEFAULT_USERNAME, DEFAULT_PASSWORD);
+        if (connection == null) {
+            logger.error("Cannot get the connection.");
+            return null;
+        }
+        return connection;
     }
 }
