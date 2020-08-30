9e0d7cc793153c55611262fb32d1e2bf152ff852
Dazikiri Anikar <mierzwinski.b@gmail.com>
2020-08-30 17:07:42 +0200

    Added initial architecture from project api-github-user-getter-a and added Constants and Connectors classes for database's connection.

---
+++ src/main/java/theapp/config/Constants.java
@@ -0,0 +1,11 @@
+package theapp.config;
+
+public class Constants {
+
+    public static final String DEFAULT_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String DEFAULT_URL = "jdbc:mysql://localhost:3306/recruitment?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    public static final String DEFAULT_USERNAME = "root";
+    public static final String DEFAULT_PASSWORD = "password";
+
+
+}
